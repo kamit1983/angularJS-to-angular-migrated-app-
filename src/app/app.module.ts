e60946ad77ba3104e772a3527356572e8c038d5a
Amit Kumar <amit.kumar@accionlabs.com>
2018-07-20 09:24:17 +0530

    partial works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {UpgradeModule} from '@angular/upgrade/static';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class Ng2AppModule {
+  constructor(public upgrade: UpgradeModule){}
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+  // import polyfills
+import 'core-js/es7/reflect'
+import 'zone.js/dist/zone'
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// import angular2 dpes
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import {Ng1AppModule} from './app/app';
+import {Ng2AppModule} from './app/app.module';
+
+/**
+ * We bootstrap the Angular 2 module first, and then, once it's done,
+ * bootstrap the Angular 1 module.
+ */
+platformBrowserDynamic().bootstrapModule(Ng2AppModule).then(ref => {
+  // bootstrap angular1
+  (<any>ref.instance).upgrade.bootstrap(document.body, [Ng1AppModule.name]);
+});
